a312819cbfabeee94351ad85e6829986caac061a
Aditya Bal Gupta <63708813+adityagupta02@users.noreply.github.com>
2023-10-01 21:42:16 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,82 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.prompts import PromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+import chainlit as cl
+
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+custom_prompt_template = """Provide a relevant answer to the user's question using insights and quote examples from the following pieces of information. If you are uncertain or unable to answer, kindly state that you don't know, and avoid making speculative responses.
+
+Context: {context}
+Question: {question}
+
+Helpful answer:
+"""
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
+
+#Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
+
+#Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    llm = CTransformers(
+        model = "TheBloke/Llama-2-7B-Chat-GGML",
+        model_type="llama",
+        max_new_tokens = 512,
+        temperature = 0.5
+    )
+    return llm
+
+#QA Model Function
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                       model_kwargs={'device': 'cpu'})
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+    return qa
+
+#output function
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({'query': query})
+    return response
+
+#chainlit on chat start code
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content = "Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Bhagvad Gita AI Bot. What is your query?"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message, callbacks=[cb])
